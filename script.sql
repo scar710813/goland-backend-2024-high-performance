0374ad7c318f236ed79a8f9b1cb1be1d18d5320c
scar710813 <unclescar0813@gmail.com>
2024-03-02 12:04:16 -0300

    feat: Add initial data to clientes table

--- script.sql
+++ script.sql
@@ -11,6 +11,13 @@ CREATE TABLE clientes (
   nome VARCHAR(100) NOT NULL,
   limite INTEGER NOT NULL
 );
+INSERT INTO clientes (nome, limite)
+  VALUES
+    ('o barato sai caro', 1000 * 100),
+    ('zan corp ltda', 800 * 100),
+    ('les cruders', 10000 * 100),
+    ('padaria joia de cocaia', 100000 * 100),
+    ('kid mais', 5000 * 100);
 
 DO $$
 BEGIN
