ced11d093e8e615a259c6c128bb54b6efd0333f9
scar710813 <unclescar0813@gmail.com>
2024-03-02 17:02:54 -0300

    fix: Refactor transaction handler and validator

--- internal/infra/webserver/handlers/transaction.go
+++ internal/infra/webserver/handlers/transaction.go
@@ -3,13 +3,13 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/infra/database"
+	usecase "github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/domain/use-case"
 	"github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/infra/dto"
+	"github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/infra/validators"
 )
 
 func TransactionHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,13 +40,13 @@ func TransactionHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	validTransaction, err := TransactionValidator(transaction.Valor, transaction.Tipo, transaction.Descricao)
+	validTransaction, err := validators.TransactionValidator(transaction.Valor, transaction.Tipo, transaction.Descricao)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	result, err := NewTransactionUseCase(
+	result, err := usecase.NewTransactionUseCase(
 		db,
 		validTransaction.Valor,
 		validTransaction.Tipo,
@@ -65,48 +65,4 @@ func TransactionHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(result)
-}
-
-func TransactionValidator(value int64, tipo string, description string) (*dto.TransactionInputDTO, error) {
-	if value <= 0 {
-		return nil, errors.New("valor da transação precisa ser positivo")
-	}
-	if tipo != "c" && tipo != "d" {
-		return nil, errors.New("tipo da transação inválido")
-	}
-	if len(description) < 1 || len(description) > 10 {
-		return nil, errors.New("descrição da transação deve ter entre 1 e 10 caracteres")
-	}
-
-	return &dto.TransactionInputDTO{
-		Valor:     value,
-		Tipo:      tipo,
-		Descricao: description,
-	}, nil
-}
-
-func NewTransactionUseCase(db *sql.DB, valor int64, tipo string, descricao string, userId int64) (*dto.TransactionOutputDTO, error) {
-	balance, err := database.GetBalanceAndLimitByUserId(db, userId)
-	if err != nil {
-		return nil, err
-	}
-
-	if tipo == "d" && valor > (balance.Total+balance.Limit) {
-		return nil, errors.New("saldo insuficiente")
-	}
-
-	err = database.CreateTransaction(db, &dto.TransactionInputDTO{
-		Valor:     valor,
-		Tipo:      tipo,
-		Descricao: descricao,
-		ClienteID: userId,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return &dto.TransactionOutputDTO{
-		Limite: balance.Limit,
-		Saldo:  balance.Total,
-	}, nil
 }
