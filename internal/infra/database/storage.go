ad462bbd8a85a99452e0ab7f516c5bf99fc08cc1
scar710813 <unclescar0813@gmail.com>
2024-03-02 09:10:47 -0300

    feat: Add MySQL support and refactor storage functions

--- internal/infra/database/storage.go
+++ internal/infra/database/storage.go
@@ -2,24 +2,20 @@ package database
 
 import (
 	"database/sql"
+	"time"
 
 	"log"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/infra/dto"
-	_ "github.com/lib/pq"
 )
 
 type Storage interface {
 	CreateTransaction(*dto.TransactionInputDTO) error
 }
 
-type PostgresStorage struct {
-	db *sql.DB
-}
-
-func NewPostgresStorage() (*PostgresStorage, error) {
-	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=rinha_de_backend_2024_q1 sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+func NewMySQLStorage() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/rinha-de-backend-mysql?parseTime=true")
 	if err != nil {
 		log.Printf("error on open connection: %v", err)
 		return nil, err
@@ -30,26 +26,111 @@ func NewPostgresStorage() (*PostgresStorage, error) {
 		return nil, err
 	}
 
-	return &PostgresStorage{
-		db: db,
-	}, nil
+	return db, nil
 }
 
-func (p *PostgresStorage) CreateTransaction(transaction *dto.TransactionInputDTO) (*dto.TransactionDTO, error) {
-	query := "INSERT INTO public.transacoes (valor, tipo, descricao, cliente_id) VALUES ($1, $2, $3, $4) RETURNING id, valor, tipo, descricao, cliente_id"
+func CreateTransaction(db *sql.DB, transaction *dto.TransactionInputDTO) error {
+	query := "INSERT INTO transacoes (valor, tipo, descricao, cliente_id, realizado_em) VALUES (?, ?, ?, ?, ?)"
 
-	stmt, err := p.db.Prepare(query)
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Printf("error on prepare statement: %v", err)
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(transaction.Valor, transaction.Tipo, transaction.Descricao, transaction.ClienteID, time.Now().Format("2006-01-02T15:04:05.999999Z"))
+	if err != nil {
+		log.Printf("error on query row: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func GetBalance(db *sql.DB, userId int64) (int64, error) {
+	query := "SELECT SUM(valor) FROM transacoes WHERE valor IS NOT NULL and cliente_id = ? and tipo = 'c'"
+	query2 := "SELECT SUM(valor) FROM transacoes WHERE valor IS NOT NULL and cliente_id = ? and tipo = 'd'"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Printf("error on prepare statement: %v", err)
+		return 0, err
+	}
+	defer stmt.Close()
+
+	var totalCredits sql.NullInt64
+	err = stmt.QueryRow(userId).Scan(&totalCredits)
+	if err != nil {
+		log.Printf("error on query row: %v", err)
+		return 0, err
+	}
+
+	stmt2, err := db.Prepare(query2)
+	if err != nil {
+		log.Printf("error on prepare statement: %v", err)
+		return 0, err
+	}
+	defer stmt2.Close()
+
+	var totalDebits sql.NullInt64
+	err = stmt2.QueryRow(userId).Scan(&totalDebits)
+	if err != nil {
+		log.Printf("error on query row: %v", err)
+		return 0, err
+	}
+
+	balance := totalCredits.Int64 - totalDebits.Int64
+
+	return balance, nil
+}
+
+func GetLimitByUserId(db *sql.DB, id int64) (int64, error) {
+	query := "SELECT limite FROM clientes WHERE id = ?"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Printf("error on prepare statement: %v", err)
+		return 0, err
+	}
+	defer stmt.Close()
+
+	var limit int64
+	err = stmt.QueryRow(id).Scan(&limit)
+	if err != nil {
+		log.Printf("error on query row: %v", err)
+		return 0, err
+	}
+
+	return limit, nil
+}
+
+func GetLastTransactionsByUserId(db *sql.DB, id int64) ([]dto.LastTransaction, error) {
+	query := "SELECT valor, tipo, descricao, realizado_em FROM transacoes WHERE cliente_id = ? ORDER BY realizado_em DESC LIMIT 10"
+
+	stmt, err := db.Prepare(query)
 	if err != nil {
 		log.Printf("error on prepare statement: %v", err)
 		return nil, err
 	}
 
-	var transactionCreated dto.TransactionDTO
-	err = stmt.QueryRow(transaction.Valor, transaction.Tipo, transaction.Descricao).Scan(&transactionCreated.ID, &transactionCreated.Valor, &transactionCreated.Tipo, &transactionCreated.Descricao)
+	rows, err := stmt.Query(id)
 	if err != nil {
 		log.Printf("error on query row: %v", err)
 		return nil, err
 	}
+	defer rows.Close()
 
-	return &transactionCreated, nil
+	var transactions []dto.LastTransaction
+	for rows.Next() {
+		var transaction dto.LastTransaction
+		err = rows.Scan(&transaction.Valor, &transaction.Tipo, &transaction.Descricao, &transaction.RealizadoEm)
+		if err != nil {
+			log.Printf("error on scan row: %v", err)
+			return nil, err
+		}
+		transactions = append(transactions, transaction)
+	}
+
+	return transactions, nil
 }
