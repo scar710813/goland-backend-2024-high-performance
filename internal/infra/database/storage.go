16654d8939f2d2021a6fe85e49c60d12b0e1c811
scar710813 <unclescar0813@gmail.com>
2024-03-02 16:46:14 -0300

    fix: Update MySQL database name in storage.go

--- internal/infra/database/storage.go
+++ internal/infra/database/storage.go
@@ -15,7 +15,7 @@ type Storage interface {
 }
 
 func NewMySQLStorage() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/rinha-de-backend-mysql?parseTime=true")
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/mysql-db?parseTime=true")
 	if err != nil {
 		log.Printf("error on open connection: %v", err)
 		return nil, err
@@ -78,7 +78,7 @@ func CreateTransaction(db *sql.DB, transaction *dto.TransactionInputDTO) error {
 	return nil
 }
 
-func GetBalanceByUserId(db *sql.DB, userId int64) (*dto.Balance, error) {
+func GetBalanceAndLimitByUserId(db *sql.DB, userId int64) (*dto.Balance, error) {
 	query := "SELECT saldo, limite FROM clientes WHERE id = ?"
 
 	stmt, err := db.Prepare(query)
@@ -99,26 +99,6 @@ func GetBalanceByUserId(db *sql.DB, userId int64) (*dto.Balance, error) {
 	balance.CreatedAt = time.Now().Format("2006-01-02T15:04:05.999999Z")
 
 	return &balance, nil
-}
-
-func GetLimitByUserId(db *sql.DB, id int64) (int64, error) {
-	query := "SELECT limite FROM clientes WHERE id = ?"
-
-	stmt, err := db.Prepare(query)
-	if err != nil {
-		log.Printf("error on prepare statement: %v", err)
-		return 0, err
-	}
-	defer stmt.Close()
-
-	var limit int64
-	err = stmt.QueryRow(id).Scan(&limit)
-	if err != nil {
-		log.Printf("error on query row: %v", err)
-		return 0, err
-	}
-
-	return limit, nil
 }
 
 func GetLastTransactionsByUserId(db *sql.DB, id int64) ([]dto.LastTransaction, error) {
