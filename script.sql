8df8ca7c661985b074a36d1ad2ada780e4ecd85f
scar710813 <unclescar0813@gmail.com>
2024-03-02 09:48:52 -0300

    fix: Refactor database schema in script.sql

--- script.sql
+++ script.sql
@@ -14,19 +14,6 @@ CREATE TABLE clientes (
 
 DO $$
 BEGIN
-  CREATE TABLE transacoes (
-    id SERIAL PRIMARY KEY,
-    valor INTEGER,
-    tipo CHAR(1) CHECK (type IN ('c', 'd')),
-    descricao VARCHAR(10),
-    cliente_id INTEGER REFERENCES clientes(id),
-    realizado_em VARCHAR(27)
-  );
-  CREATE TABLE clientes (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    limite INTEGER NOT NULL
-  );
   INSERT INTO clientes (nome, limite)
   VALUES
     ('o barato sai caro', 1000 * 100),
