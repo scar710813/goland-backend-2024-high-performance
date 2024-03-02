530af268ad46fca1dcdea55fae4e305b125cabc0
scar710813 <unclescar0813@gmail.com>
2024-03-02 09:10:51 -0300

    feat: Add new DTO structs for balance and last transactions

--- internal/infra/dto/dto.go
+++ internal/infra/dto/dto.go
@@ -19,3 +19,21 @@ type TransactionOutputDTO struct {
 	Limite int64 `json:"limite"`
 	Saldo  int64 `json:"saldo"`
 }
+
+type Balance struct {
+	Total     int64  `json:"total"`
+	CreatedAt string `json:"data_extrato"`
+	Limit     int64  `json:"limite"`
+}
+
+type LastTransaction struct {
+	Valor       int64  `json:"valor"`
+	Tipo        string `json:"tipo"`
+	Descricao   string `json:"descricao"`
+	RealizadoEm string `json:"realizado_em"`
+}
+
+type ExtractOutputDTO struct {
+	Balance          Balance           `json:"saldo"`
+	LastTransactions []LastTransaction `json:"ultimas_transacoes"`
+}
