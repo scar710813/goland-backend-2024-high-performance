a4522b8c5a798c5463b0e3e6144c839bcd1df254
scar710813 <unclescar0813@gmail.com>
2024-03-07 10:12:01 -0300

    fix: Refactor create-transaction-use-case.go to use entity.Transaction

--- internal/domain/use-case/create-transaction-use-case.go
+++ internal/domain/use-case/create-transaction-use-case.go
@@ -4,33 +4,36 @@ import (
 	"database/sql"
 	"errors"
 
+	"github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/domain/entity"
 	"github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/infra/database"
 	"github.com/kleytonsolinho/rinha-de-backend-2024-q1/internal/infra/dto"
 )
 
 func NewTransactionUseCase(db *sql.DB, valor int64, tipo string, descricao string, userId int64) (*dto.TransactionOutputDTO, error) {
+	entityTransaction := entity.NewTransaction(valor, tipo, descricao)
+
 	balance, err := database.GetBalanceAndLimitByUserId(db, userId)
 	if err != nil {
 		return nil, err
 	}
 
-	if balance.Total+balance.Limit <= valor && tipo == "d" {
+	if balance.Total+balance.Limit <= entityTransaction.Valor && entityTransaction.Tipo == "d" {
 		return nil, errors.New("saldo insuficiente")
 	}
 
 	go func() {
 		_ = database.CreateTransaction(db, &dto.TransactionInputDTO{
-			Valor:     valor,
-			Tipo:      tipo,
-			Descricao: descricao,
+			Valor:     entityTransaction.Valor,
+			Tipo:      entityTransaction.Tipo,
+			Descricao: entityTransaction.Descricao,
 			ClienteID: userId,
 		})
 	}()
 
 	if tipo == "c" {
-		balance.Total = balance.Total + valor
+		balance.Total = balance.Total + entityTransaction.Valor
 	} else {
-		balance.Total = balance.Total - valor
+		balance.Total = balance.Total - entityTransaction.Valor
 	}
 
 	return &dto.TransactionOutputDTO{
